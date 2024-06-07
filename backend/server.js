c1188e83e338d9bf6420ea5f5ee335196248b71a
Hansika <nostochk@gmail.com>
2024-06-07 22:22:09 +0530

    connect to data base

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
 
-app.listen(3000,()=>{
-    console.log("Server is listening on port 3000");
-})
+app.listen(process.env.PORT, () => {
+  console.log(`Server is listening on port ${process.env.PORT}`);
+});
