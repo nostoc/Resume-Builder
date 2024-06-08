3441d6f7c39a9c05db44d0fd4d6171797636e63d
Hansika <nostochk@gmail.com>
2024-06-08 13:12:39 +0530

    complete sign-in api route

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,7 +1,8 @@
-import express from 'express';
-import { signup } from '../controllers/auth.controller.js';
+import express from "express";
+import { signin, signup } from "../controllers/auth.controller.js";
 const router = express.Router();
 
-router.post("/sign-up",signup)
+router.post("/sign-up", signup);
+router.post("/sign-in", signin);
 
 export default router;
