e45a809deec567fa0848a25c353a67e46f254b16
Hansika <nostochk@gmail.com>
2024-06-22 12:09:28 +0530

    fix cors configuration

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
 import express from "express";
 import cors from "cors";
-import bodyparser from "body-parser";
+
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.js";
 import profileRoutes from "./routes/profile.js";
 import resumeRoutes from "./routes/resume.js";
 import connectDB from "./config/db.js";
-import mongoose from "mongoose";
+
 //load env variables
 dotenv.config();
 
@@ -21,9 +21,21 @@ app.use((req, res, next) => {
 });
 //init middlewares
 app.use(express.json());
+
+//cors config
+const allowedOrigins = ['http://localhost:5173', 'https://eazyrezume.senani.me/'];
+
 app.use(cors({
-  origin: 'http://localhost:5173', 
-  credentials: true
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true,
 }));
 
 // Define Routes (we will add routes later)
